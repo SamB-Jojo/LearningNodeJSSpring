ccd27627e3857e81097336ef1087ac41d25a97d0
SamB-Jojo <SamB-Jojo@users.noreply.github.com>
2016-02-24 15:43:18 -0500

    Added files via upload
    The files for the Spring Building a RESTful Web Service Guide

---
+++ Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by sams on 2/24/2016.
+ */
+ import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
